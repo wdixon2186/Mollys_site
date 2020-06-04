89666b843e77080e6624de53c320a2a84b946c5f
wdixon2186 <53952397+wdixon2186@users.noreply.github.com>
2020-06-04 02:01:12 -0600

    progress

--- html-template/contact.php
+++ html-template/contact.php
@@ -57,7 +57,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@themeforest.net";
+$address = "wdixon2186@gmail.com";
 
 
 // Configuration option.
